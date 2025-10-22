667bfbc3e01d92b900dfb62b5f32c103abf7d9d0
Cristian <titix1994@gmail.com>
2025-10-22 19:33:08 -0300

    agregue rama de fetch donde voy a estar trabajando

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
 var nombre:string = "pepe";
 console.log(nombre)
+
+import type { ILogin } from "./types/ILogin";
+
+const BASE_URL = "http://localhost:8080/api/auth";
+
+export async function loginUser(data: ILogin) {
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al iniciar sesión");
+  }
+
+  return response.json();
+}
---
+++ src/types/ILogin.ts
@@ -0,0 +1,6 @@
+export interface ILogin {
+  email: string;
+  password: string;
+  nombre: string;
+  apellido: string;
+}
