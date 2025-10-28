3465a7e0dcf0c5f7c1f2f88236b99c8035e06dd3
Martin Guerra Local <vernonguerra27@gmail.com>
2025-10-28 11:30:25 -0300

    Arregle redireccion del cliente y del administrador

--- src/utils/navigate.ts
+++ src/utils/navigate.ts
@@ -2,7 +2,7 @@
 import type { SessionUser } from './auth';
 
 export function goToRoleHome(user: SessionUser) {
-  if (user.role === 'admin') {
+  if (user.rol === 'admin') {
     window.location.replace('/src/pages/admin/adminHome/adminHome.html');
   } else {
     window.location.replace('/src/pages/store/home/home.html');
